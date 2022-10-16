739dc9f3a4bb2152eee1ffe29c18afec27db5762
HichamDz38 <dachirhicham@gmail.com>
2022-10-16 06:15:03 +0100

    creat the ceaser functions

---
+++ ceaser_cipher.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# in this first lab we are going to create a encryption/decryption program
+
+
+def encrypt(message, key=3):
+    """return the cipher Message based on the given Message and Key"""
+    #write your code here
+
+def decrypt(cipher, key=3):
+    """return the Plain Message based on the given Cipher_Message and Key"""
+    #write your code here
