1067abf6778c54d2508eee9dcba8c79877e4de8c
HichamDz38 <dachirhicham@gmail.com>
2022-10-18 09:10:37 +0100

    add my_lib

---
+++ LAB2/my_lib.py
@@ -0,0 +1,14 @@
+
+
+def get_total_case_by_country(country):
+    pass
+
+
+def get_monthly_new_case(country):
+    pass
+
+def available_countries():
+    pass
+
+def get_total_case_by_continent(continent):
+    pass
